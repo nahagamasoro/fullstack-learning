65cf740598d61b77e881c284158ec10dde0400c8
Nahagama Lazeni Soro <nahagama.dev@gmail.com>
2022-12-17 12:17:01 +0000

    Part2/courseinfo

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,41 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part part={part} key={part.id} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const exercisesNumber = parts.reduce((s, p) => s + p.exercises, 0);
+
+  return <p>total of {exercisesNumber} exercises</p>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+
+      <Content parts={course.parts} />
+
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
